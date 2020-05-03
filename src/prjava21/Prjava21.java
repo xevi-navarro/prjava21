1fb84e4b061bd7b3cad79b052161681248430604
Xevi Navarro <xevi.navarro@buzzmn.com>
2020-05-03 13:01:49 +0200

    Versió 0.1 del projecte prjava21

---
+++ src/prjava21/Prjava21.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava21;
+
+import java.io.*;
+
+public class Prjava21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");
+            bw.newLine();
+            bw.write("  <head>");
+            bw.newLine();
+            bw.write("    <title>");
+            bw.newLine();
+            bw.write("      Nova p&agrave;gina  web");
+            bw.newLine();
+            bw.write("    </title>");
+            bw.newLine();
+            bw.write("  </head>");
+            bw.newLine();
+            bw.write("  <body>");
+            bw.newLine();
+            bw.write("    Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write("  </body>");
+            bw.newLine();
+            bw.write("</html>");
+            bw.newLine();
+            bw.close();
+        }
+    }
+}
