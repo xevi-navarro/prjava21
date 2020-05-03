d77a1bd7ed5e895eac68fd9bb1810e7f38796f72
Xevi Navarro <xevi.navarro@buzzmn.com>
2020-05-03 13:18:32 +0200

    Versió 0.2 del projecte prjava21

--- src/prjava21/Prjava21.java
+++ src/prjava21/Prjava21.java
@@ -28,6 +28,7 @@ public class Prjava21 {
             bw.newLine();
             bw.write("  <body>");
             bw.newLine();
+            bw.write(" <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
             bw.write("    Nova p&agrave;gina web");
             bw.newLine();
             bw.write("  </body>");
